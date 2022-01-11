806d74e13f2a9a9e32cf2f8940ce051459155c46
gitcoin_yoon <socozy7210@gmail.com>
2022-01-11 17:52:39 +0900

    add resources for smartcontract_lottery

--- solidity_course/freecodecamp_org/smartcontract_lottery/scripts/check_contract.py
+++ solidity_course/freecodecamp_org/smartcontract_lottery/scripts/check_contract.py
@@ -1,15 +1,28 @@
 
-from brownie import Lottery, config, network, accounts
+from brownie import Lottery, config, network
+from scripts.deploy_handler import get_account, get_contract
 
 def check_value():
 
-    account = accounts[0]
+    account = get_account()
     contract = Lottery.deploy(
 
-        config["networks"][network.show_active()]["eth_usd_price_feed"],
-        {"from": account}
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False)
     )
 
+    (fee, ethPrice) = contract.getPrice()
+    adjustedPrice = ethPrice * 10**10
+    cost = fee/adjustedPrice * 10**18
+    print("test")
+    print(cost)
+    print("contract")
     print(contract.getEntranceFee())
 
 def main():
