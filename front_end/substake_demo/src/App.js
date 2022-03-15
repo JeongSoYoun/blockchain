fd8635e9ff2ba58fed2a1fe8e7f57350192cea04
gitcoin_yoon <socozy7210@gmail.com>
2022-03-15 21:22:19 +0900

    substake

--- front_end/substake_demo/src/App.js
+++ front_end/substake_demo/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Link from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import StakingView from "./StakingView";
@@ -6,8 +7,10 @@ import HomeView from "./HomeView";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsMobile } from "./features/windowSizer/windowSlice";
 import { selectMenu } from "./features/menuSelector/menuSlice";
+import Divider from "@mui/material/Divider";
 import "./WalletHandler";
 import "./App.css";
+
 window.WalletExtension.subscribeMetaMask();
 
 function App() {
@@ -40,6 +43,13 @@ function App() {
         {/* Home & Dashboard */}
 
         {menuSelector.main === "home" ? <HomeView /> : <StakingView />}
+      </div>
+
+      <div className="app-footer">
+        <Divider sx={{ backgroundColor: "#7b7b7b" }} />
+        <div className="footer-title">
+          <h2> &copy;SUBSTAKE</h2>
+        </div>
       </div>
     </div>
   );
