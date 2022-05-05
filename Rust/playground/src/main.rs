c7cda55ba28e2b031ae35ffcaa2dd2762dfba9fb
gitcoin_yoon <socozy7210@gmail.com>
2022-05-05 18:27:11 +0900

    commit

--- Rust/playground/src/main.rs
+++
@@ -1,9 +0,0 @@
-use playground::List::{Cons, Nil};
-use std::rc::Rc;
-
-fn main() {
-
-    let a = Rc::new(Cons(1, Rc::new(Nil)));
-    let b = Rc::new(Cons(2, Rc::clone(&a)));
-    println!("{:?}", b);
-}
